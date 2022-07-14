24b30b68fbd6432aacc2e7d8740b7f1f217859a4
Eduardo <eduardotakeuchi@outlook.com>
2022-07-14 10:56:34 -0300

    First Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,10 @@
+import NotificationButton from './components/NotificationButton'
 function App() {
   return (
-    <h1>Olá mundo</h1>
+    <>
+      <h1>Olá mundo</h1>
+      <NotificationButton />
+    </>
   )
 }
 
---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,13 @@
+
+import icon from "../../assets/notification-icon.svg"
+function NotificationButton() {
+  return (
+    <>
+      <div className="dsmeta-red-btn">
+        <img src={icon} alt="Notificar" />
+      </div>
+    </>
+  )
+}
+
+export default NotificationButton
