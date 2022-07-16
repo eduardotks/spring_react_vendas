8a84a0e30cbf8ab6e305e9396d49ee364b50204c
Eduardo <eduardotakeuchi@outlook.com>
2022-07-16 17:52:27 -0300

    Links

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -6,9 +6,15 @@ function Header() {
             <div className="dsmeta-logo-container">
                 <img src={iconLogo} alt="DSMeta" />
                 <h1>DSMeta</h1>
+                
                 <p>
                     Desenvolvido por
-                    <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
+                    <a href="https://www.instagram.com/devsuperior.ig"> @devsuperior.ig</a>
+                </p>
+                <br/>
+                <p>
+                    Executado por
+                    <a href="https://github.com/eduardotks"> Eduardo Tks</a>
                 </p>
             </div>
         </header>
