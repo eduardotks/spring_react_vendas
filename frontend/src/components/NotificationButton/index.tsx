8e9aec971bac8a50a3a31a51e256b34f79ed752f
Eduardo <eduardotakeuchi@outlook.com>
2022-07-16 16:32:39 -0300

    Send Notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,22 @@
 
 import icon from "../../assets/notification-icon.svg"
-function NotificationButton() {
+import axios from 'axios';
+import{ BASE_URL } from '../../utils/request';
+
+type Props = {
+  saleId: number;
+}
+
+function handleClick(id : number) {
+  axios(`${BASE_URL}/sales/${id}/notify`).then((response) => {
+    console.log(response);
+  })
+}
+
+function NotificationButton({saleId}: Props) {
   return (
     <>
-      <div className="dsmeta-red-btn">
+      <div className="dsmeta-red-btn" onClick={()=> handleClick(saleId)}>
         <img src={icon} alt="Notificar" />
       </div>
     </>
